a7e2512e6aebc24aa8dc9b347ae6c36435badd65
Christian Alexander Vazquez Gonzalez <dev.chrisvaz@gmail.com>
2024-05-23 10:24:07 -0600

    correction added

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,18 +8,5 @@ def factorial(n):
         n = n - 1
     return result
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py <integer>")
-    sys.exit(1)
-
-try:
-    num = int(sys.argv[1])
-    if num < 0:
-        print("Error: The input must be a non-negative integer.")
-        sys.exit(1)
-except ValueError:
-    print("Error: The input must be an integer.")
-    sys.exit(1)
-
-f = factorial(num)
+f = factorial(int(sys.argv[1]))
 print(f)
